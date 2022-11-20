ec6d93b42183e280dcae74b888b0d53c3af48262
Nicolas Renault <contact.nicolasrenault@gmail.com>
2022-11-20 22:40:58 +0100

    Add the selection with arrows

--- cheaty.js
+++ cheaty.js
@@ -16,16 +16,23 @@ document.onkeydown = (e) => {
 		} else {
 			initSelectMode();
 		}
-	} else if (e.code == "ArrowUp" && selectMode) { // ArrowUp
+	} else if (e.code == "ArrowUp" && selectMode) { // Arrow Up
 		e.preventDefault();
 		selectParentComponent();
-    } else if (e.code == "ArrowDown" && selectMode) { // ArrowDown
+    } else if (e.code == "ArrowDown" && selectMode) { // Arrow Down
 		e.preventDefault();
 		selectChildComponent();
+    } else if (e.code == "ArrowLeft" && selectMode) { // Arrow Left
+		e.preventDefault();
+		selectPreviousSiblingComponent();
+    } else if (e.code == "ArrowRight" && selectMode) { // Arrow Right
+		e.preventDefault();
+		selectNextSiblingComponent();
     } else if (e.code == "Escape" && selectMode) { // Escape
 		e.preventDefault();
 		stopSelectMode();
 	} else if (e.code == "Enter") { // Enter
+		validateSelection();
 	}
 };
 
@@ -75,10 +82,47 @@ function selectComponent() {
 }
 
 /**
+ * Select the parent of the current component
+ */
+function selectParentComponent() {
+	if (currentComponent == null || currentComponent.parentNode == null) return;
+	currentComponent = currentComponent.parentNode;
+	addBorderToCurrentComponent();
+}
+
+/**
+ * Select the child of the current component
+ */
+function selectChildComponent() {
+	if (currentComponent == null || currentComponent.firstChild == null) return;
+	currentComponent = currentComponent.firstChild;
+	addBorderToCurrentComponent();
+}
+
+/**
+ * Select the previous sibling of the current component
+ */
+function selectPreviousSiblingComponent() {
+	if (currentComponent == null || currentComponent.previousSibling == null) return;
+	currentComponent = currentComponent.previousSibling;
+	addBorderToCurrentComponent();
+} 
+
+/**
+ * Select the next sibling of the current component
+ */
+function selectNextSiblingComponent() {
+	if (currentComponent == null || currentComponent.nextSibling == null) return;
+	currentComponent = currentComponent.nextSibling;
+	addBorderToCurrentComponent();
+} 
+
+/**
  * Add a border to the current component selected
  */
 function addBorderToCurrentComponent() {
 	removeAllBorder();
+	console.log(currentComponent);
 	currentComponent.classList.add(CSS_CLASS_NAME);
 }
 
