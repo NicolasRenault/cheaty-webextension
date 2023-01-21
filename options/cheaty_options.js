df0b02b7b1909e137b2d1b90f11de9836fcb06a5
Nicolas Renault <contact.nicolasrenault@gmail.com>
2023-01-21 19:50:08 +0100

    Add CSS for option page

--- options/cheaty_options.js
+++ options/cheaty_options.js
@@ -6,32 +6,56 @@
 function saveOptions(e) {
 	e.preventDefault();
 
-	chrome.storage.sync.set({
-		inspectorMode: document.querySelector("#inspector-mode").checked,
-	});
+	try {
+		chrome.storage.sync.set({
+			inspectorMode: document.querySelector("#inspector-mode").checked,
+		});
+	} catch (error) {
+		logError(error);
+	}
 }
 
 /**
  * Get data from storage sync and set it in the form
  */
 function restoreOptions() {
-	function setCurrentChoice(result) {
-		document.querySelector("#inspector-mode").checked =
-			result.inspectorMode || false;
-	}
-
-	function onError(error) {
-		document.querySelector("#msg").innerHTML += `Error: ${error}`;
-	}
-
-	// chrome.storage.sync.get("inspectorMode").then(setCurrentChoice, onError); //? This is not working in Firefox
-	chrome.storage.sync.get("inspectorMode", function (items) {
-		if (!chrome.runtime.error) {
-			console.log(items);
-			setCurrentChoice(items);
+	try {
+		function setCurrentChoice(result) {
+			document.querySelector("#inspector-mode").checked =
+				result.inspectorMode || false;
 		}
-	});
+
+		// chrome.storage.sync.get("inspectorMode").then(setCurrentChoice, onError); //? This is not working in Firefox
+		chrome.storage.sync.get("inspectorMode", function (items) {
+			if (!chrome.runtime.error) {
+				console.log(items);
+				setCurrentChoice(items);
+			}
+		});
+	} catch (error) {
+		logError(error);
+	}
+}
+
+/**
+ * Display the error message
+ *
+ * @param {string|Error} message
+ */
+function logError(message) {
+	console.error(message);
+
+	let cheaty_error = document.getElementById("cheaty_error");
+	cheaty_error.classList.add("visible");
+
+	let logs = document.getElementById("logs");
+	let error = document.createElement("pre");
+	error.innerHTML = message;
+
+	logs.appendChild(error);
 }
 
 document.addEventListener("DOMContentLoaded", restoreOptions);
-document.querySelector("form").addEventListener("submit", saveOptions);
+document
+	.getElementById("inspector-mode")
+	.addEventListener("change", saveOptions);
