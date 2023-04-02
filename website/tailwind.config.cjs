7db26fd1bd07ca35933c005161a33a9f43fdb88c
Nicolas Renault <contact.nicolasrenault@gmail.com>
2023-04-02 16:34:21 +0200

    Add header + title

--- website/tailwind.config.cjs
+++ website/tailwind.config.cjs
@@ -2,7 +2,18 @@
 module.exports = {
 	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 	theme: {
-		extend: {},
+		colors: {
+			primary: "#F2E9E4",
+			primaryLight: "#FFFEFC",
+			primaryDark: "#B9ADA7",
+			secondary: "#22223B",
+			secondaryLight: "#4A4E69",
+			tertiary: "#9A8C98",
+			tertiaryLight: "#C9ADA7",
+		},
+		fontFamily: {
+			sans: ["Poppins", "sans-serif"],
+		},
 	},
 	plugins: [],
 };
